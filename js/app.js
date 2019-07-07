eab23b9a912315b99ff9f9cb00ac930cd4b3066d
Jonathon Schwamman <schwamman@msn.com>
2019-07-07 10:49:32 -0500

    Changed to use question/response arrays for questions 1-5

--- js/app.js
+++ js/app.js
@@ -3,50 +3,66 @@ var userName = prompt('What is you name?');
 var numCorrect = 0;
 var sevenCorrect = false;
 
-runGame();
-alert('Good game ' + userName + ', you got ' + numCorrect + ' out of 7 questions correct. Refresh to play again!');
+//Creates arrays for questions 1-5
+var questionArray = ['Did I graduate college?', 'Do I have any pets?', 'Are lagers my favorite style of beer?', 'Have I always lived in Iowa?', 'Is my favorite sport soccer?'];
+var yesResponses = ['Yes, I graduated from the University of Northern Iowa in 2016.', 'Yes, I have two cats named Titan and Brownie.', 'That is incorrect, I actually prefer IPAs.', 'Yes, I have travelled outside the state but I have always lived in either Cedar Rapids or Cedar Falls.', 'Nope, my favorite sport is hockey. I\'m a Colorado Avalanche fan.'];
+var noResponses = ['That is incorrect, I graduated from the University of Northern Iowa in 2016.', 'That is incorrect, I have two cats named Titan and Brownie.', 'That is correct, I actually prefer IPAs.', 'That is incorrect, I have travelled outside the state but I have always lived in either Cedar Rapids or Cedar Falls.', 'That correct, my favorite sport is hockey. I\'m a Colorado Avalanche fan.'];
+var answerArray = [];
+var questionIndex = 0;
 
+
+runGame();
 
 function runGame() {
-  questionOne();
-  questionTwo();
-  questionThree();
-  questionFour();
-  questionFive();
+  for (questionIndex = 0; questionIndex < 5; questionIndex++) {
+    if (questionIndex === 0) {
+      questionOne();
+    } else if (questionIndex === 1) {
+      questionTwo();
+    }
+    else if (questionIndex === 2) {
+      questionThree();
+    }
+    else if (questionIndex === 3) {
+      questionFour();
+    } else {
+      questionFive();
+    }
+  }
   questionSix();
   questionSeven();
+  alert('Good game ' + userName + ', you got ' + numCorrect + ' out of 7 questions correct. Refresh to play again!');
 }
 
 function questionOne() {
-  var answerOne = prompt('Did I graduate college?').toLowerCase().trim();
-  if (answerOne === 'y' || answerOne === 'yes') {
-    alert('Yes, I graduated from the University of Northern Iowa in 2016.');
+  answerArray[questionIndex] = prompt(questionArray[questionIndex]).toLowerCase().trim();
+  if (answerArray[questionIndex] === 'y' || answerArray[questionIndex] === 'yes') {
+    alert(yesResponses[questionIndex]);
     numCorrect++;
-  } else if (answerOne === 'n' || answerOne === 'no') {
-    alert('That is incorrect, I graduated from the University of Northern Iowa in 2016.');
+  } else if (answerArray[questionIndex] === 'n' || answerArray[questionIndex] === 'no') {
+    alert(noResponses[questionIndex]);
   } else {
     alert('Please enter yes or no.');
   }
 }
-
 function questionTwo() {
-  var answerTwo = prompt('Do I have any pets?').toLowerCase().trim();
-  if (answerTwo === 'y' || answerTwo === 'yes') {
-    alert('Yes, I have two cats named Titan and Brownie.');
+  answerArray[questionIndex] = prompt(questionArray[questionIndex]).toLowerCase().trim();
+  if (answerArray[questionIndex] === 'y' || answerArray[questionIndex] === 'yes') {
+    alert(yesResponses[questionIndex]);
     numCorrect++;
-  } else if (answerTwo === 'n' || answerTwo === 'no') {
-    alert('That is incorrect, I have two cats named Titan and Brownie.');
+  } else if (answerArray[questionIndex] === 'n' || answerArray[questionIndex] === 'no') {
+    alert(noResponses[questionIndex]);
   } else {
     alert('Please enter yes or no.');
   }
 }
 
 function questionThree() {
-  var answerThree = prompt('Are lagers my favorite style of beer?').toLowerCase().trim();
-  if (answerThree === 'y' || answerThree === 'yes') {
-    alert('That is incorrect, I actually prefer IPAs.');
-  } else if (answerThree === 'n' || answerThree === 'no') {
-    alert('That is correct, I actually prefer IPAs.');
+  answerArray[questionIndex] = prompt(questionArray[questionIndex]).toLowerCase().trim();
+  if (answerArray[questionIndex] === 'y' || answerArray[questionIndex] === 'yes') {
+    alert(yesResponses[questionIndex]);
+  } else if (answerArray[questionIndex] === 'n' || answerArray[questionIndex] === 'no') {
+    alert(noResponses[questionIndex]);
     numCorrect++;
   } else {
     alert('Please enter yes or no.');
@@ -54,23 +70,23 @@ function questionThree() {
 }
 
 function questionFour() {
-  var answerFour = prompt('Have I always lived in Iowa?').toLowerCase().trim();
-  if (answerFour === 'y' || answerFour === 'yes') {
-    alert('Yes, I have travelled outside the state but I have always lived in either Cedar Rapids or Cedar Falls.');
+  answerArray[questionIndex] = prompt(questionArray[questionIndex]).toLowerCase().trim();
+  if (answerArray[questionIndex] === 'y' || answerArray[questionIndex] === 'yes') {
+    alert(yesResponses[questionIndex]);
     numCorrect++;
-  } else if (answerFour === 'n' || answerFour === 'no') {
-    alert('That is incorrect, I have travelled outside the state but I have always lived in either Cedar Rapids or Cedar Falls.');
+  } else if (answerArray[questionIndex] === 'n' || answerArray[questionIndex] === 'no') {
+    alert(noResponses[questionIndex]);
   } else {
     alert('Please enter yes or no.');
   }
 }
 
 function questionFive() {
-  var answerFive = prompt('Is my favorite sport soccer?').toLowerCase().trim();
-  if (answerFive === 'y' || answerFive === 'yes') {
-    alert('Nope, my favorite sport is hockey. I\'m a Colorado Avalanche fan.');
-  } else if (answerFive === 'n' || answerFive === 'no') {
-    alert('That correct, my favorite sport is hockey. I\'m a Colorado Avalanche fan.');
+  answerArray[questionIndex] = prompt(questionArray[questionIndex]).toLowerCase().trim();
+  if (answerArray[questionIndex] === 'y' || answerArray[questionIndex] === 'yes') {
+    alert(yesResponses[questionIndex]);
+  } else if (answerArray[questionIndex] === 'n' || answerArray[questionIndex] === 'no') {
+    alert(noResponses[questionIndex]);
     numCorrect++;
   } else {
     alert('Please enter yes or no.');
@@ -100,9 +116,9 @@ function questionSix() {
 
 function questionSeven() {
   for (var i = 6; i > 0; i--) {
-    var answerArray = ['hyundai', 'subaru', 'volkswagen', 'chrysler'];
+    var makeArray = ['hyundai', 'subaru', 'volkswagen', 'chrysler'];
     var answerSeven = prompt('I have owned 4 different vehicles. Guess the brand/make for any of my cars?').toLowerCase().trim();
-    var checkIfTrue = answerArray.includes(answerSeven);
+    var checkIfTrue = makeArray.includes(answerSeven);
     if (checkIfTrue) {
       alert('You are correct. I did own a ' + answerSeven + '. Keep guessing for other makes.');
       sevenCorrect = true;
@@ -116,10 +132,3 @@ function questionSeven() {
     numCorrect++;
   }
 }
-
-
-
-
-
-
-
